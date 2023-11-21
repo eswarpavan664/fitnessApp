20dccbc4ebee86470ac1da29f9f3edf27594b46f
ganesh-nallamilli <nunganesh46@gmail.com>
2023-11-21 23:44:18 +0530

    minor changes

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -10,6 +10,9 @@ const DashboardContainer = styled.div`
 
 const Dashboard = () => {
   const [result, setResult] = useState(null)
+  const [fromDate, setFromDate] = useState('');
+  const [toDate, setToDate] = useState('');
+  const [filteredData, setFilteredData] = useState([]);
 
   useEffect(() => {
     const token = localStorage.getItem('token');
@@ -19,7 +22,7 @@ const Dashboard = () => {
 
     axios.get(apiUrl)
       .then(response => {
-        console.log('Response:', response.data);
+        // console.log('Response:', response.data);
         setResult(response.data);
       })
       .catch(error => {
@@ -27,11 +30,39 @@ const Dashboard = () => {
       });
   }, []);
 
-  console.log(result)
+  useEffect(() => {
+
+    const token = localStorage.getItem('token');
+    const [header, payload, signature] = token.split('.');
+    const decodedPayload = JSON.parse(atob(payload));
+
+    const fetchData = async () => {
+      try {
+        if (fromDate && toDate) {
+          const apiUrl = 'http://localhost:5000/user/dashboard/track_filters';
+
+          const response = await axios.post(apiUrl, {
+            email: decodedPayload?.email,
+            fromDate: fromDate,
+            toDate: toDate,
+          });
+
+          setFilteredData(response.data);
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+
+  }, [fromDate, toDate])
+
+  // console.log(result)
 
   return (
 
-    <div className='' style={{ height: "89vh", background: "white", padding: "10px", margin: "10px" }}>
+    <div className='' style={{ height: "auto", background: "white", padding: "10px", margin: "10px" }}>
       <div style={{ backgroundImage: `url(${banner})`, height: "43vh", position: "relative" }}>
         <h1 style={{ position: "absolute", bottom: 20, fontSize: "3rem", left: 30, fontWeight: "bold", color: "white", textShadow: "0 0 10px black" }}>DASHBOARD</h1>
       </div>
@@ -96,26 +127,51 @@ const Dashboard = () => {
 
       </div>
 
-      <div className='row align-items-center mt-5'>
-        <div className='col-4 px-5'>
-          <div className='border d-flex justify-content-between align-items-center w-100 p-5 dash_box' style={{ background: 'linear-gradient(to right, #9BBEC8 85%, #427D9D 50%)' }} >
-            <h4 className='m-0 p-0'>Total Sleep Time (mins)</h4>
-            <p className='m-0 p-0 text-light'>{result?.totalSleepTime || ""}</p>
+      <div>
+        <div class="date-picker-container">
+          <div className='pick-dates'>
+            <div class="date-input-container ms-3" style={{display:"flex", flexDirection:"column"}}>
+              <label>From Date:</label>
+              <input type="date" class="date-input" value={fromDate} onChange={(e) => setFromDate(e.target.value)} />
+            </div>
+            <div class="date-input-container ms-5" style={{display:"flex", flexDirection:"column"}}>
+              <label>To Date:</label>
+              <input type="date" class="date-input" value={toDate} onChange={(e) => setToDate(e.target.value)} />
+            </div>
           </div>
-        </div>
-        <div className='col-4'>
-          <div className='border d-flex justify-content-between align-items-center w-100 p-5 dash_box' style={{ background: 'linear-gradient(to right, #9BBEC8 85%, #427D9D 50%)' }} >
-            <h4 className='m-0 p-0'>Water intake (ltrs)</h4>
-            <p className='m-0 p-0 text-light'>{result?.totalWaterIntake || ""}</p>
+
+
+          <div>
+            <div class="dashboard-container">
+
+              <div class="metric-card">
+                <h3>Total Water Intake</h3>
+                <p class="metric-value">{filteredData?.totalWaterIntake || "-"} liters</p>
+              </div>
+
+              <div class="metric-card">
+                <h3>Total Sleep Time</h3>
+                <p class="metric-value">{filteredData?.totalSleepTime || "-"} hours</p>
+              </div>
+
+              <div class="metric-card">
+                <h3>Total Steps</h3>
+                <p class="metric-value">{filteredData?.totalSteps || "-"}</p>
+              </div>
+
+              <div class="metric-card">
+                <h3>Total Count</h3>
+                <p class="metric-value">{filteredData?.totalCount || "-"}</p>
+              </div>
+            </div>
           </div>
+
+
+
         </div>
-        <div className='col-4'>
-          <div className='border d-flex justify-content-between align-items-center w-100 p-5 dash_box' style={{ background: 'linear-gradient(to right, #9BBEC8 85%, #427D9D 50%)' }} >
-            <h4 className='m-0 p-0'>Total seps</h4>
-            <p className='m-0 p-0 text-light'>{result?.totalSteps || ""}</p>
-          </div>
-        </div>
+
       </div>
+
     </div>
 
   );
